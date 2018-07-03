1ff6d5eeda2fa126b0f2f1079bcc1d9afe446b4e
陈晓拉尼 <2524729719@qq.com>
2018-07-03 19:46:20 +0800

    页脚开发ok

--- news-app/src/js/components/mobile_pc_index.js
+++ news-app/src/js/components/mobile_pc_index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MobilePCHeader from './mobile_pc_header';
+import MobilePCFooter from './mobile_pc_footer'
  
 export default class MobilePCIndex extends React.Component {
     render() {
         return (
            <div>
                <MobilePCHeader/>
+               <MobilePCFooter/>
            </div> 
         )
     }
